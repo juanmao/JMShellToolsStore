89a6baf3aa6879fd2c7bab3d403716dfd18f577f
huzhongqiang <huzhongqiang@meituan.com>
2013-09-25 09:46:29 +0800

    my first sh

---
+++ forMyself.sh
@@ -0,0 +1,14 @@
+# this is just some words for myself
+# I am not a sh but a daily checkins
+
+# come to github
+# recite words
+# reading
+# running
+# thinking
+
+# 
+#!/bin/bash
+
+echo "hello world"
+
